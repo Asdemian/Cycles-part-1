53f24bcb7d88da41e7aa0e0a2ff5ee1f05651feb
hw commit <demaniks@yahoo.com>
2022-11-10 18:57:31 +0300

    Задание 1-2

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,9 @@ public class Main {
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i+ " год является високосным.");
         }
+        //task 1-2
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
     }
 }
