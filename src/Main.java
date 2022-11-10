137b8f19d9eafcf4c0ff742e41ec4fa5ad7b7d25
hw commit <demaniks@yahoo.com>
2022-11-10 18:36:12 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //task 1
+        //task1
         for (int i = 1; i <= 10; i ++) {
-                System.out.println( "Итерация цикла " +i);
-            }
+            System.out.println( "Итерация цикла " +i);
+        }
+        //task 2
+        for (int i = 10; i >= 0; i --) {
+            System.out.println( "Итерация цикла " +i);
+        }
     }
 }
